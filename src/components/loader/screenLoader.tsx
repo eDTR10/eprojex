1e7bada79ed1effc5ab4dca56f6b0e78ef5dbbea
iannico322 <iannicocaulin@gmail.com>
2025-04-10 15:42:57 +0800

    ok

--- src/components/loader/screenLoader.tsx
+++ src/components/loader/screenLoader.tsx
@@ -1,7 +1,7 @@
 import './loader.css'
 const Loaderx = () => {
   return (
-    <div className='flex items-center justify-center w-full h-full bg-background '>
+    <div className='flex items-center justify-center w-screen h-screen bg-background '>
 <svg className='loading' viewBox="25 25 50 50">
   <circle className='loading-circle' r="20" cy="50" cx="50"></circle>
 </svg>
